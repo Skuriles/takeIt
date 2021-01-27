c25f187d74eba89cfb9a552ffd134e616bc9842f
Bene Caspari <skuriles@gmx.de>
2021-01-27 05:13:44 +0100

    resource renaming

--- Scripts/Player/CompositeSprites.gd
+++ Scripts/Player/CompositeSprites.gd
@@ -1,65 +1,46 @@
 extends Node
 
-var body_sprites = [
-	preload("res://Assets/Characters/Idle_Body.png")
-]
-
-var arm_sprites = [
-	preload("res://Assets/Characters/Idle_Arms.png"),
-	preload("res://Assets/Characters/Idle_Arms1.png"),
-	preload("res://Assets/Characters/Idle_Arms2.png"),
-	preload("res://Assets/Characters/Idle_Arms3.png"),
-	preload("res://Assets/Characters/Idle_Arms4.png"),
-	preload("res://Assets/Characters/Idle_Arms5.png"),
-	preload("res://Assets/Characters/Idle_Arms6.png"),
-	preload("res://Assets/Characters/Idle_Arms7.png")
-]
-
-var shoe_sprites = [
-	preload("res://Assets/Characters/Idle_Shoes1_1.png"),
-	preload("res://Assets/Characters/Idle_Shoes1_2.png")	
-]
+var body_sprites = []
+var arm_sprites = []
+var shoe_sprites = []
+var lower_sprites = []
+var upper_sprites = []
+var hair_sprites = []
+var moustache_sprites = []
 
 
-var lower_sprites = [
-	preload("res://Assets/Characters/Idle_Trouser1.png"),
-	preload("res://Assets/Characters/Idle_Trouser2.png"),
-	preload("res://Assets/Characters/Idle_Trouser3.png")
-]
+func _ready():
+	loadBodySprites()
 
-var upper_sprites = [
-	preload("res://Assets/Characters/Idle_Shirt1.png"),
-	preload("res://Assets/Characters/Idle_Shirt2.png"),
-	preload("res://Assets/Characters/Idle_Shirt3.png"),
-	preload("res://Assets/Characters/Idle_Shirt4.png"),
-	preload("res://Assets/Characters/Idle_Shirt5.png"),
-	preload("res://Assets/Characters/Idle_Shirt6.png"),
-	preload("res://Assets/Characters/Idle_Shirt6_1.png"),	
-]
 
-var hair_sprites = [
-	preload("res://Assets/Characters/Idle_Hair1_1.png"),
-	preload("res://Assets/Characters/Idle_Hair1_2.png"),
-	preload("res://Assets/Characters/Idle_Hair1_3.png"),
-	preload("res://Assets/Characters/Idle_Hair1_4.png"),
-	preload("res://Assets/Characters/Idle_Hair2_1.png"),
-	preload("res://Assets/Characters/Idle_Hair2_2.png"),
-	preload("res://Assets/Characters/Idle_Hair2_3.png"),
-	preload("res://Assets/Characters/Idle_Hair2_4.png")	
-]
-
-var moustache_sprites = [
-	preload("res://Assets/Characters/Idle_Moustache1_1.png"),
-	preload("res://Assets/Characters/Idle_Moustache1_2.png"),
-	preload("res://Assets/Characters/Idle_Moustache1_3.png"),
-	preload("res://Assets/Characters/Idle_Moustache1_4.png"),
-	preload("res://Assets/Characters/Idle_Moustache2_1.png"),
-	preload("res://Assets/Characters/Idle_Moustache2_2.png"),
-	preload("res://Assets/Characters/Idle_Moustache2_3.png"),
-	preload("res://Assets/Characters/Idle_Moustache2_4.png"),
-	preload("res://Assets/Characters/Idle_Moustache3_1.png"),
-	preload("res://Assets/Characters/Idle_Moustache3_2.png"),
-	preload("res://Assets/Characters/Idle_Moustache3_3.png"),
-	preload("res://Assets/Characters/Idle_Moustache3_4.png")	
-]
-
+func loadBodySprites():
+	var path = "res://Assets/Characters"
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "":
+			#break the while loop when get_next() returns ""
+			break
+		elif (
+			! file_name.begins_with(".")
+			and ! file_name.ends_with(".import")
+			and file_name.ends_with(".png")
+		):
+			var preloadPath = path + "/" + file_name
+			if "Body" in file_name:
+				body_sprites.append(load(preloadPath))
+			if "Arm" in file_name:
+				arm_sprites.append(load(path + "/" + file_name))
+			if "Shoe" in file_name:
+				shoe_sprites.append(load(path + "/" + file_name))
+			if "Lower" in file_name:
+				lower_sprites.append(load(path + "/" + file_name))
+			if "Upper" in file_name:
+				upper_sprites.append(load(path + "/" + file_name))
+			if "Hair" in file_name:
+				hair_sprites.append(load(path + "/" + file_name))
+			if "Moustache" in file_name:
+				moustache_sprites.append(load(path + "/" + file_name))
+	dir.list_dir_end()
