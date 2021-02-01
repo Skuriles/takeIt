431b6378e1ad5a484adc08b5fa1431117fc04b32
Bene Caspari <skuriles@gmx.de>
2021-02-01 16:23:18 +0100

    kiosk und trigger

--- Scripts/Player/TriggerArea.gd
+++ Scripts/Player/TriggerArea.gd
@@ -7,7 +7,10 @@ onready var player = get_parent() as KinematicBody2D
 onready var animPlayer = get_parent().find_node("AnimationPlayer") as AnimationPlayer
 
 var player_old_z_index = 0
+var body_old_index = 0
+var wall_old_index = 0
 var counter: int = 0
+var wall_counter: int = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,15 +21,26 @@ func _ready():
 
 
 func _on_tilemap_enter(body):
-	if body.is_in_group("objects") or body.is_in_group("walls"):
+	if body.is_in_group("objects"):
+		body_old_index = body.z_index
 		counter += 1
-		print(counter)
-		player.z_index = body.z_index + 1
+		# player.z_index = body.z_index + 1
+		body.z_index = player.z_index - 1
+	if body.is_in_group("walls"):
+		wall_old_index = body.z_index
+		wall_counter += 1
+		# player.z_index = body.z_index + 1
+		body.z_index = player.z_index - 1
 
 
 func _on_tilemap_exit(body):
-	if body.is_in_group("objects") or body.is_in_group("walls"):
+	if body.is_in_group("objects"):
 		counter -= 1
-		print(counter)
 		if counter == 0:
-			player.z_index = player_old_z_index
+			body.z_index = body_old_index
+			# player.z_index = player_old_z_index
+	if body.is_in_group("walls"):
+		wall_counter -= 1
+		if wall_counter == 0:
+			body.z_index = wall_old_index
+			# player.z_index = player_old_z_index
